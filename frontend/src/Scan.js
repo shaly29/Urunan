62259f6b8738d383747a4dea14f5f93cdf3aa065
shaly29 <nishalinithirukkumar2@gmail.com>
2024-03-27 22:16:16 +0530

    changes

--- frontend/src/Scan.js
+++ frontend/src/Scan.js
@@ -4,15 +4,15 @@ const Scan = () => {
     return (
         <div>
 
-            <div className="col-lg-4" style={{margin:"200px 600px ",width:'500px'}}>
-                <div className="blog-box">
-                    <img src={require("./assets/images/scan.jpeg")} />
+<div className="col-lg-4" style={{ margin: "20px auto", maxWidth: '300px' }}> {/* Center-align and limit maximum width */}
+  <div className="blog-box">
+    <img src={require("./assets/images/scan.jpeg")} style={{ width: '100%', height: 'auto' }} /> {/* Make image responsive */}
+    <div className="blog-text">
+      {/* Add your blog text content here */}
+    </div>
+  </div>
+</div>
 
-                    <div className="blog-text">
-
-                    </div>
-                </div>
-            </div>
             
 
         </div>
